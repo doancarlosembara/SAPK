97c8f8f092a4edf288c8545b77de8677149a15ba
doancarlosembara <carlosdoanembara@gmail.com>
2023-07-27 19:40:49 +0700

    final

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b><a href="https://simian.co.id/">PT Simian Technologies</a></b>
+    </div>
+    <strong>SAPK (Sistem Absensi dan Payroll Karyawan)</strong>
+</footer>
